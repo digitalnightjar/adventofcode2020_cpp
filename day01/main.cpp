7e122c560392aebd26bf69817354f5747a1242bd
Karl Eyre <digitalnightjar@outlook.com>
2020-12-02 00:03:19 +0000

    Part 02.

--- day01/main.cpp
+++ day01/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstdint>
 #include <stdexcept>
+#include <unordered_set>
 
 const std::vector<uint32_t> readData(const std::string& path)
 {
@@ -40,14 +41,11 @@ const std::vector<uint32_t> readData(const std::string& path)
     return data;
 }
 
-int main(int, char**)
+const uint32_t getPartOne(const std::vector<uint32_t>& data, const uint32_t target, bool& success)
 {
     uint32_t result = 0;
-    bool success = false;
-    const std::string filePath = "./values.txt";
+    success = false;
 
-    std::cout << "Hello, AdventOfCode 2020!\n";
-    const std::vector<uint32_t> data = readData(filePath);
     const size_t dataLen = data.size();
 
     for (size_t i = 0; i < dataLen; ++i)
@@ -56,21 +54,73 @@ int main(int, char**)
         for (size_t j = 0; j < dataLen; ++j)
         {
             uint32_t b = data.at(j);
-            if (a + b == 2020)
+            if (a + b == target)
             {
                 success = true;
                 result = a*b;
+                return result;
             }
         }
     }
 
+    return result;
+}
+
+const uint32_t getPartTwo(const std::vector<uint32_t>& data, const uint32_t target, bool& success)
+{
+    uint32_t result = 0;
+    // Brute force probably wont work this time, as we need to find the product of 3 numbers(triplet) where the sum of the set is 2020.
+    // There's an approach to do this using a few ptrs, but a hash approach is probably easier to follow.
+    const size_t dataLen = data.size();
+
+    // Fix the first element as A[i] 
+    for (int i = 0; i < dataLen - 2; ++i)  
+    { 
+        // Find pair in subarray data[i+1..n-1] 
+        // with sum equal to sum - data[i] 
+        std::unordered_set<uint32_t> dataSet; 
+        uint32_t curr_sum = target - data.at(i); 
+        for (uint32_t j = i + 1; j < dataLen; j++)  
+        { 
+            if (dataSet.find(curr_sum - data.at(j)) != dataSet.end())  
+            { 
+                // success
+                uint32_t A = data.at(i);
+                uint32_t B = data.at(j);
+                uint32_t C = curr_sum - data.at(j);
+                // Double checking we have 3 values that equal 2020
+                if ((A + B + C) == 2020)
+                {
+                    result = A * B * C;
+                    std::cout << "Triplet is: " << A << " " << B << " " << C << "\n";
+                    success &= true;
+                    return result; 
+                }
+            } 
+            dataSet.insert(data.at(j)); 
+        } 
+    } 
+  
+    // failure
+    success = false;
+    return result;
+}
+
+int main(int, char**)
+{
+    uint32_t result = 0;
+    bool success = false;
+    const std::string filePath = "./values.txt";
+
+    std::cout << "Hello, AdventOfCode 2020!\n";
+    const std::vector<uint32_t> data = readData(filePath);
+
+    const uint32_t partOneResult = getPartOne(data, 2020, success);
+    const uint32_t partTwoResult = getPartTwo(data, 2020, success);
     if (success)
     {
-        std::cout << "The answer is: " << result << "\n";
-    }
-    else
-    {
-        std::cout << "Couldn't find a matching pair, the code is probably wrong!\n";
+        std::cout << "Part One answer is: " << partOneResult << "\n";
+        std::cout << "Part Two answer is: " << partTwoResult << "\n";
     }
 
     return result;
